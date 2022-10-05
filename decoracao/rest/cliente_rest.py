6072bead6aafc0844f5ebeb7a12b4483291cf482
conceicao <conceicao-peres@hotmail.com>
2022-10-05 16:54:01 -0300

    Pesquisar um cliente #5 - finalizado

--- decoracao/rest/cliente_rest.py
+++ decoracao/rest/cliente_rest.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter, status, Response
+
 from decoracao.persistencia.db import connect_db
-from decoracao.modelos.modelos_cliente import Item
+from decoracao.modelos.modelos_cliente import Item, ItemEmail
 from decoracao.regras.cliente_regras import valida_email
+
+import urllib.parse
 import datetime
+
 
 # Minha rota API de cliente
 rota_cliente = APIRouter(
@@ -12,7 +16,7 @@ rota_cliente = APIRouter(
 
 # Cria novo cliente
 @rota_cliente.post(
-    "/api/cliente/",
+    "/api/cliente",
     response_model=Item,
     # Ajustado o código HTTP de retorno
     status_code=status.HTTP_201_CREATED
@@ -34,3 +38,20 @@ async def criar_novo_cliente(item: Item, response: Response):
         print("Não pode se conectar ao servidor.")
 
     return req
+
+
+@rota_cliente.get(
+    "/api/busca/cliente/{email}",
+    response_model=Item,
+    status_code=status.HTTP_200_OK
+)
+
+async def pesquisar_pelo_email(email: str):
+    req = urllib.parse.unquote(email)
+    result = connect_db().cliente.find_one({"email": req})
+    return result
+
+
+
+
+
