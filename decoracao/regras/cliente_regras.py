c799d86499d68873786958229f9fc160c3ec9cad
conceicao <conceicao-peres@hotmail.com>
2022-10-08 21:30:37 -0300

    Novo layout.

--- decoracao/regras/cliente_regras.py
+++ decoracao/regras/cliente_regras.py
@@ -1,11 +1,4 @@
-from decoracao.persistencia.db import connect_db
 
-def valida_email(req):
-    result = connect_db().cliente.find_one({
-        "email": req['email']
-    })
-
-    return result
 
 
 
--- decoracao/rest/cliente_rest.py
+++ decoracao/rest/cliente_rest.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, status, Response
 
 from decoracao.persistencia.db import connect_db
+from decoracao.persistencia.obter_colecoes import colecao_cliente
 from decoracao.modelos.modelos_cliente import Item, ItemEmail
-from decoracao.regras.cliente_regras import valida_email
+from decoracao.persistencia.cliente_persistencia import valida_email, busca_por_email, cadastrar_cliente
 
 import urllib.parse
 import datetime
-
 
 # Minha rota API de cliente
 rota_cliente = APIRouter(
@@ -22,21 +22,16 @@ rota_cliente = APIRouter(
     status_code=status.HTTP_201_CREATED
 )
 async def criar_novo_cliente(item: Item, response: Response):
-
     req = {
         'nome': item.nome,
         'email': item.email,
         'criacao': datetime.datetime.utcnow()
     }
-    try:
-        if valida_email(req) is not None:
-            response.status_code = status.HTTP_409_CONFLICT
-        else:
-            # noinspection PyStatementEffect
-            connect_db().cliente.insert_one(req).inserted_id
-    except connect_db.errors.ConnectionFailure:
-        print("Não pode se conectar ao servidor.")
 
+    if valida_email(req) is not None:
+        response.status_code = status.HTTP_409_CONFLICT
+    else:
+        cadastrar_cliente(req)
     return req
 
 
@@ -45,10 +40,8 @@ async def criar_novo_cliente(item: Item, response: Response):
     response_model=Item,
     status_code=status.HTTP_200_OK
 )
-
 async def pesquisar_pelo_email(email: str):
-    req = urllib.parse.unquote(email)
-    result = connect_db().cliente.find_one({"email": req})
+    result = busca_por_email(email)
     return result
 
 
@@ -56,8 +49,6 @@ async def pesquisar_pelo_email(email: str):
     "/api/remove/cliente/{email}",
     status_code=status.HTTP_204_NO_CONTENT
 )
-
 async def remove_cliente(email):
     if valida_email({"email": email}) is not None:
-        connect_db().cliente.delete_one({"email": email})
-
+        colecao_cliente.delete_one({"email": email})
