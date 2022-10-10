3cd45f1b371fe89730b31d4e0a5abc8ed5222865
conceicao <conceicao-peres@hotmail.com>
2022-10-10 15:30:00 -0300

    finalizando feature/restructuring

--- decoracao/rest/cliente_rest.py
+++ decoracao/rest/cliente_rest.py
@@ -3,7 +3,9 @@ from fastapi import APIRouter, status, Response
 from decoracao.persistencia.db import connect_db
 from decoracao.persistencia.obter_colecoes import colecao_cliente
 from decoracao.modelos.modelos_cliente import Item, ItemEmail
-from decoracao.persistencia.cliente_persistencia import valida_email, busca_por_email, cadastrar_cliente
+from decoracao.regras.cliente_regras import cadastrar_novo_cliente
+from decoracao.persistencia.cliente_persistencia import (
+    valida_email, busca_por_email, cadastrar_cliente, remover_cliente)
 
 import urllib.parse
 import datetime
@@ -27,13 +29,9 @@ async def criar_novo_cliente(item: Item, response: Response):
         'email': item.email,
         'criacao': datetime.datetime.utcnow()
     }
+    cadastrar_novo_cliente(req)
 
-    if valida_email(req) is not None:
-        response.status_code = status.HTTP_409_CONFLICT
-    else:
-        cadastrar_cliente(req)
     return req
-
 
 @rota_cliente.get(
     "/api/busca/cliente/{email}",
@@ -44,11 +42,10 @@ async def pesquisar_pelo_email(email: str):
     result = busca_por_email(email)
     return result
 
-
 @rota_cliente.delete(
     "/api/remove/cliente/{email}",
     status_code=status.HTTP_204_NO_CONTENT
 )
 async def remove_cliente(email):
     if valida_email({"email": email}) is not None:
-        colecao_cliente.delete_one({"email": email})
+        remover_cliente(email)
