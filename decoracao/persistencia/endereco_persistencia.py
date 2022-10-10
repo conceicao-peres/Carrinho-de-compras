87a3071d1808a2f272a3ff0b8970f70bebe50d37
Juliana <juliana.sade84@gmail.com>
2022-10-10 20:15:59 -0300

    ajuste pesquisa endereco

--- decoracao/persistencia/endereco_persistencia.py
+++ decoracao/persistencia/endereco_persistencia.py
@@ -1,10 +1,10 @@
 from decoracao.persistencia.obter_colecoes import colecao_endereco
-from decoracao.persistencia.cliente_persistencia import busca_por_email
+import urllib.parse
 
 def cadastrar_endereco(req): colecao_endereco.insert_one(req)
 
 
 def busca_por_email(email: str):
     req = urllib.parse.unquote(email)
-    result = colecao_cliente.find_one({"email": req})
+    result = colecao_endereco.find_one({"email": req})
     return result
