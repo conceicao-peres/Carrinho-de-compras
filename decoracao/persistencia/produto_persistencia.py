125ccab35a4a18ab4729fb055e42bb3fcdfaa226
Marina Servo Morales <marinaservomorales@gmail.com>
2022-10-10 22:30:57 -0300

    commit marina feature/pesquisar_produto

--- decoracao/persistencia/produto_persistencia.py
+++ decoracao/persistencia/produto_persistencia.py
@@ -1,7 +1,21 @@
 from decoracao.persistencia.obter_colecoes import colecao_produto
-
-
-def cadastrar_produto(req):
-    colecao_produto.insert_one(req).inserted_id
-
-
+from bson import ObjectId
+import urllib.parse
+ 
+ 
+def busca_por_id(_id: str):
+    req = urllib.parse.unquote(_id)
+    result = colecao_produto.find_one({
+        "_id": ObjectId(req)
+    })
+ 
+    return result
+ 
+ 
+def valida_nome_produto(nome_produto: str):
+    req = urllib.parse.unquote(nome_produto)
+    result = colecao_produto.find_one({
+        "nome_produto": req
+    })
+ 
+    return result
--- decoracao/rest/produto_rest.py
+++ decoracao/rest/produto_rest.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status, Response
 from decoracao.modelos.modelos_produtos import Product
-from decoracao.persistencia.produto_persistencia import cadastrar_produto
+from decoracao.persistencia.produto_persistencia import busca_por_id,valida_nome_produto
 
 # Minha rota API de produto
 rota_produto = APIRouter(
@@ -9,29 +9,25 @@ rota_produto = APIRouter(
 )
 
 
-@rota_produto.post(
-    "/api/produto/",
+@rota_produto.get(
+    "/api/busca/produto/{_id}",
     response_model=Product,
-    # Ajustado o código HTTP de retorno
-    status_code=status.HTTP_201_CREATED
+    status_code=status.HTTP_200_OK
 )
-async def criar_novo_produto(produto: Product, response: Response):
-    req = {
-        'nome_produto': produto.nome_produto,
-        'descricao_produto': produto.descricao_produto,
-        'qtde_produto': produto.qtde_produto,
-        'preco_produto': produto.preco_produto,
-        'qtde_estoque': produto.qtde_estoque,
-        'material_produto': produto.material_produto,
-    }
-    # try:
-    #     if valida_codigo_produto(req) is not None:
-    #         response.status_code = status.HTTP_409_CONFLICT
-    #     else:
-    #         print("passei aqui 3")
-    cadastrar_produto(req)
-    # except connect_db.errosproduto.ConnectionFailure:
-    #     print("Não pode se conectar ao servidor.")
+async def pesquisar_pelo_id(_id: str):
+    result = busca_por_id(_id)
+    return result
+ 
+ 
+ 
+ 
+@rota_produto.get(
+    "/api/busca/produto/nome/{nome_produto}",
+    response_model=Product,
+    status_code=status.HTTP_200_OK
+)
+async def pesquisar_pelo_nome(nome_produto: str):
+    result = valida_nome_produto(nome_produto)
+    return result
 
-    return req
 
