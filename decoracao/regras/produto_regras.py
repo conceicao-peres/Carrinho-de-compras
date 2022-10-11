c515ed4d78cabbd8f961665944716b2393c95428
Marina Servo Morales <marinaservomorales@gmail.com>
2022-10-10 21:55:38 -0300

    commit marina cadastrar produto

--- decoracao/regras/produto_regras.py
+++ decoracao/regras/produto_regras.py
@@ -1,4 +1,3 @@
-from bson import ObjectId
 from decoracao.persistencia.obter_colecoes import colecao_produto
 import urllib.parse
 
@@ -7,15 +6,6 @@ def valida_nome_produto(nome_produto: str):
     req = urllib.parse.unquote(nome_produto)
     result = colecao_produto.find_one({
         "nome_produto": req
-    })
-
-    return result
-
-
-def valida_id_produto(_id: str):
-    req = urllib.parse.unquote(_id)
-    result = colecao_produto.find_one({
-        "_id": ObjectId(req)
     })
 
     return result
