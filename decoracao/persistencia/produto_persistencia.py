494676205b56406f978effe71b59cd3586365d75
Marina Servo Morales <marinaservomorales@gmail.com>
2022-10-11 21:46:52 -0300

    commit marina feature/pesquisar_produto + deletar_produto

--- decoracao/persistencia/produto_persistencia.py
+++ decoracao/persistencia/produto_persistencia.py
@@ -2,7 +2,12 @@ from decoracao.persistencia.obter_colecoes import colecao_produto
 from bson import ObjectId
 import urllib.parse
  
+
+def cadastrar_produto(req):
+    colecao_produto.insert_one(req)
  
+
+
 def busca_por_id(_id: str):
     req = urllib.parse.unquote(_id)
     result = colecao_produto.find_one({
@@ -19,3 +24,7 @@ def valida_nome_produto(nome_produto: str):
     })
  
     return result
+
+
+def remover_produto(_id):
+    colecao_produto.delete_one({"_id": ObjectId(_id)})
--- decoracao/rest/produto_rest.py
+++ decoracao/rest/produto_rest.py
@@ -1,6 +1,7 @@
+from bson import ObjectId
 from fastapi import APIRouter, status, Response
 from decoracao.modelos.modelos_produtos import Product
-from decoracao.persistencia.produto_persistencia import busca_por_id,valida_nome_produto
+from decoracao.persistencia.produto_persistencia import cadastrar_produto, busca_por_id,valida_nome_produto, remover_produto
 
 # Minha rota API de produto
 rota_produto = APIRouter(
@@ -8,6 +9,31 @@ rota_produto = APIRouter(
     prefix=""
 )
 
+@rota_produto.post(
+    "/api/produto/",
+    response_model=Product,
+    # Ajustado o código HTTP de retorno
+    status_code=status.HTTP_201_CREATED
+)
+async def criar_novo_produto(produto: Product, response: Response):
+    req = {
+        'nome_produto': produto.nome_produto,
+        'descricao_produto': produto.descricao_produto,
+        'qtde_produto': produto.qtde_produto,
+        'preco_produto': produto.preco_produto,
+        'qtde_estoque': produto.qtde_estoque,
+        'material_produto': produto.material_produto
+    }
+
+
+    if valida_nome_produto(req['nome_produto']) is not None:
+        response.status_code = status.HTTP_409_CONFLICT
+    else:
+        cadastrar_produto(req)
+ 
+
+
+    return req
 
 @rota_produto.get(
     "/api/busca/produto/{_id}",
@@ -31,3 +57,11 @@ async def pesquisar_pelo_nome(nome_produto: str):
     return result
 
 
+@rota_produto.delete(
+    "/api/remove/produto/{_id}",
+    status_code=status.HTTP_204_NO_CONTENT
+)
+async def remove_produto_pelo_id(_id: str):
+    result = busca_por_id(_id)
+    if result is not None:
+        remover_produto(_id)
