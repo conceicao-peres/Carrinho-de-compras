671a37234315e7e51b162d06c09a0d2d7595d53e
conceicao <conceicao-peres@hotmail.com>
2022-10-31 10:31:47 -0300

    padronizacao address

--- decoracao/rest/rest_conf.py
+++ decoracao/rest/rest_conf.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 from decoracao.rest.cliente_rest import user
 from decoracao.rest.produto_rest import rota_produto
 from decoracao.rest.carrinho_rest import rota_carrinho
-from decoracao.rest.endereco_rest import rota_enderecos
+from decoracao.rest.endereco_rest import address
 
 def configurar_rotas(app: FastAPI):
     app.include_router(user)
     app.include_router(rota_produto)
     app.include_router(rota_carrinho)
-    app.include_router(rota_enderecos)
+    app.include_router(address)
 
 def criar_aplicacao_fastapi():
     app = FastAPI(title="Shop Cart",
