b516c691b2e855df93bbc30031140ef74bc99bab
Consu Peres <73192544+conceicao-peres@users.noreply.github.com>
2022-11-03 15:22:22 -0300

    Update test_cliente.py

--- testes/test_cliente.py
+++ testes/test_cliente.py
@@ -4,7 +4,7 @@ import json
 
 @pytest.mark.asyncio
 async def test_criar_novo_cliente():
-    payload = json.dumps({'nome': 'juju', 'email': 'juju@hotmail.com'})
+    payload = json.dumps({'nome': 'maria', 'email': 'maria@hotmail.com'})
     response = requests.post('http://localhost:8000/user', payload)
     status_code = response.status_code
     assert status_code == 201
@@ -23,7 +23,7 @@ async def test_pesquisar_por_email_inexistente():
 
 @pytest.mark.asyncio
 async def test_criar_novo_cliente_email_existente():
-    payload = json.dumps({'nome': 'juju', 'email': 'maria@hotmail.com'})
+    payload = json.dumps({'nome': 'maria', 'email': 'maria@hotmail.com'})
     response = requests.post('http://localhost:8000/user', payload)
     status_code = response.status_code
     assert status_code == 409
@@ -36,7 +36,7 @@ async def test_remove_cliente_inexistente():
 
 @pytest.mark.asyncio
 async def test_remove_cliente():
-    response = requests.delete('http://localhost:8000/user/juju@hotmail.com')
+    response = requests.delete('http://localhost:8000/user/maria@hotmail.com')
     status_code = response.status_code
     assert status_code == 204
 
